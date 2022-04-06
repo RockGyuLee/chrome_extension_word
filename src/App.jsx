6a11f793f638edc65c1d84b7051427a9954075ca
RockGyuLee <rmfoszld@naver.com>
2022-04-06 18:18:31 +0900

    webpack => vite change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import React, { useState, useEffect} from 'react';
-import { ThemeProvider } from "styled-components";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { useState } from 'react'
+import { Layout } from './Components/Layout'
+import Rock from "../imgs/rock.png"
+import './App.css'
 
-//modules
-import "./style/App.css";
-import "./style/common.styl";
-import theme from "./theme.js";
-import EngWordsPage from "./pages/WordPage";
-import Header from "./pages/Logo";
+import dataJson from "../"
 
-export default function App(){
-
-    return(
-        <ThemeProvider theme={theme}>
-            <Header/>
-            <EngWordsPage/>
-        </ThemeProvider>
-    )
+function App() {
+  return (
+    <Layout>
+       <img src={Rock} className="App-logo" alt="logo" />
+       <div> </div>
+    </Layout>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={Rock} className="App-logo" alt="logo" />
+    //       <p>Hello Vite + React!</p>
+        /* <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p> */
+  )
 }
+
+export default App
---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import styled from "styled-components";
+
+export const ButtonGrp = () => {
+
+}
+
+export const Button = styled.div`
+
+`
